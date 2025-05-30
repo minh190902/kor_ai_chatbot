c3419ebd571f5f6e02b41f280c0e998c2a1ab5bb
AI-DEV\AI-DEVELOPER <minhnguyen@keduall.com>
2025-05-30 08:49:55 +0700

    update test_db

--- test/test_db.py
+++ test/test_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("chatbot.db")
+conn = sqlite3.connect("src/db/chatbot.db")
 cursor = conn.cursor()
 for row in cursor.execute("SELECT * FROM chat_messages"):
     print(row)
