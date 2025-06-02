6fd2ae769a990d93bdf8a4413b8f64c18ccd982d
AI-DEV\AI-DEVELOPER <minhnguyen@keduall.com>
2025-06-02 17:04:58 +0700

    create api for chatbot

---
+++ api/endpoints/general_conversation.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
+from ..schemas import ChatbotRequest, ChatbotResponse, ErrorResponse
+from ...src.db.db_config import init_db
+
+from src.models.kor_learning import KorLearningModels
+
+router = APIRouter()
+
+@router.post(
+    "/chat_response",
+    response_model=str,
+    responses={500: {"model": ErrorResponse}},
+)
+def request_chatbot(request: ChatbotRequest):
+    try:
+        state = {
+            "user_id": request.user_id,
+            "session_id": request.session_id,
+            "message": request.message,
+            "history": request.history,
+            "level": request.level
+        }
+        model = KorLearningModels(
+            user_id=state["user_id"],
+            sesstion_id=state["session_id"],
+            level=state["level"]
+        )
+        response = model.chat(state["message"])
+        return StreamingResponse(
+            iter([response["response"]]),
+            media_type="text/plain"
+        )
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code = 500,
+            detail= f"Something went wrong, retry again later or contact support. Error: {str(e)}"
+        )
