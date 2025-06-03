416046372850c2771df9b4c9fc04310bf7891671
AI-DEV\AI-DEVELOPER <minhnguyen@keduall.com>
2025-06-03 14:02:07 +0700

    remove unneccesary file and add example env

--- api/schemas/errors.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class ErrorResponse(BaseModel):
-    error_code: str
-    detail: str
--- test/test_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("src/db/chatbot.db")
-cursor = conn.cursor()
-for row in cursor.execute("SELECT * FROM chat_messages"):
-    print(row)
-conn.close()
